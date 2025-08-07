b3ff0389810d8d8bdab336425ad84a929fc7c265
Ahmed Ameer <ahmedameerbwp2@gmail.com>
2025-08-07 12:07:51 +0500

    basics of javaScript

---
+++ JSFoundation/part3/condition-challanges.js
@@ -0,0 +1,59 @@
+// Checking if a number is greater than another number:
+
+let num1 = 5;
+let num2 = 11;
+
+/*
+console.log("HEllo i am the upper one");
+if (num1 > num2) {
+  console.log("num 1 is greater than num 2");
+} else {
+  console.log("num1 is smaller than num 2");
+}
+console.log("Program Ends!");
+*/
+
+// Checking if a string is equal to another string:
+/*
+let user = "Ahmed";
+let username = "Ahmed";
+
+if (user == username) {
+  console.log("Username not available");
+} else {
+  console.log("username available!");
+} */
+
+// Checking if a variable is a number or not:
+/*
+let score = 1;
+
+if (typeof score === "number") {
+  console.log("yes its a number");
+} else {
+  console.log("No ! its not a number");
+}
+*/
+
+// Checking if a boolean value is true or false:
+/*
+let isLoggedIn = true;
+
+if (isLoggedIn) {
+    console.log("Yes True");
+} else{
+    console.log("false");
+}
+*/
+
+// Checking if an array is empty or not:
+
+let list = [];
+
+console.log(list.length);
+
+if (list.length == 0) {
+  console.log("Yes! the array is empty");
+} else {
+  console.log("No! the array is not empty");
+}
