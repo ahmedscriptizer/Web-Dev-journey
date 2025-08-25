07b31cbdad30577c568cca09e754dee5d807904f
Ahmed Ameer <ahmedameerbwp2@gmail.com>
2025-08-25 17:00:22 +0500

    practice 5 challenges on DOM

--- JSFoundation/part6/script.js
+++ JSFoundation/part6/script.js
@@ -1,3 +1,64 @@
-console.log("Hello from script");
-console.log(window);
-console.log(document.getElementsByTagName("h1"));
+// challenge # 1
+document
+  .getElementById("change-text-btn")
+  .addEventListener("click", function () {
+    let paragraph = document.getElementById("text-paragraph");
+    paragraph.innerText =
+      "Ahmed Ameer is CEO of biggest sofware indusry in Pakistan";
+  });
+
+// challenge # 2
+
+function addhighlight(index) {
+  let cities = document.querySelectorAll("#city-list li");
+
+  cities.forEach((city) => {
+    city.style.color = "";
+    city.style.fontWeight = "";
+  });
+
+  cities[index].style.color = "yellow";
+  cities[index].style.fontWeight = "bold";
+}
+
+document
+  .getElementById("highlight-city-btn")
+  .addEventListener("click", function () {
+    addhighlight(2);
+  });
+
+// Example # 3
+
+document
+  .getElementById("change-order-btn")
+  .addEventListener("click", function () {
+    document.getElementById("order-text").innerText = "Order: Espresso";
+  });
+
+// Example # 4
+
+function additem(item) {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(`${item}`));
+  document.querySelector("#shopping-list").appendChild(li);
+
+  /* li.innerText = `${item}`;
+  document.getElementById("shopping-list").appendChild(li); */
+}
+
+document.getElementById("add-item-btn").addEventListener("click", function () {
+  additem("chocolate");
+});
+
+// Example # 5
+
+function removeItem(item) {
+  const task = document.querySelectorAll("#task-list li");
+  task[0].remove();
+}
+
+document
+  .getElementById("remove-task-btn")
+  .addEventListener("click", function () {
+    removeItem(0);
+  });
