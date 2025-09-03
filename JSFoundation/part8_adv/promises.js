1c896849f0d5b06a569d4c66f9d5fd2ff3c17af9
Ahmed Ameer <ahmedameerbwp2@gmail.com>
2025-09-03 08:40:02 +0500

    Fetch practice done with promise and async await

--- JSFoundation/part8_adv/promises.js
+++ JSFoundation/part8_adv/promises.js
@@ -48,9 +48,35 @@ PromiseOne.then(function (data) {
 async function consumedPromiseOne() {
   try {
     let response = await PromiseOne;
-    console.log(response);
+    console.log(`Async: ${response}`);
   } catch (error) {
     console.log(error);
   }
 }
 consumedPromiseOne();
+
+// ================  API REQUEST =========================================
+
+/* async function getUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(`User: ${data[0].name}`);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+}
+getUsers(); */
+
+// .then .catch format
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data[1].name);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
