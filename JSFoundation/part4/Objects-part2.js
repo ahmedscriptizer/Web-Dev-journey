d95b18848d17b1478faedeac1c02dfb6849da09a
Ahmed Ameer <ahmedameerbwp2@gmail.com>
2025-09-04 12:39:31 +0500

    de-structure Objects

--- JSFoundation/part4/Objects-part2.js
+++ JSFoundation/part4/Objects-part2.js
@@ -10,25 +10,42 @@ const User = {
     },
   },
 };
-console.log(User.UserfullName.InApp.LocalSaved.name);
+// console.log(User.UserfullName.InApp.LocalSaved.name);
 
-// combining Objects
+// ================= combining Objects into one ==========================
 
 const obj1 = { name: "Ahmed", age: 21 };
 const obj2 = { location: "Bahawalpur", Device: "Lenovo" };
 
 let obj3 = Object.assign({}, obj1, obj2);
-console.log(obj3);
+// console.log(obj3);
+
+// ======================== getting keys and values ========================
 
 // getting keys which is useful for DB, projects
 //  v.vIMP
 
 const Student = {
-    name: "Muhammad Ahmed Ameer ",
-    class: "Bscs 5b",
-    Roll : 2315
-}
+  name: "Muhammad Ahmed Ameer ",
+  class: "Bscs 5b",
+  Roll: 2315,
+};
 
 let keys = Object.keys(Student);
-console.log(keys);  // It gives an array 
+// console.log(keys);  // It gives an array
+
+//  ==============  DeStructuring of Objects ==================
+
+const course = {
+  name: "data-science",
+  duration: "3-months",
+  InstructorName: "Hitesh choudhry",
+};
+
+const { InstructorName } = course;
+console.log(InstructorName);
+// short the name
+const { InstructorName: instructor } = course;
+console.log(instructor);
+
 
