b3b595cdd7afd235d93803045de4e0617d424477
Ahmed Ameer <ahmedameerbwp2@gmail.com>
2025-10-18 10:48:06 +0500

    cart functionality

---
+++ JSFoundation/JS_Projects/03_ecommerce/script.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const products = [
+    { id: 1, name: "headphone", price: 34.99 },
+    { id: 2, name: "keyboard", price: 19.99 },
+    { id: 3, name: "laptop", price: 75.99 },
+  ];
+  const cart = [];
+
+  const productList = document.getElementById("product-list");
+  const cartItems = document.getElementById("cart-items");
+  const emptyCartMessage = document.getElementById("empty-cart");
+  const cartTotalMessage = document.getElementById("cart-total");
+  const totalPriceDisplay = document.getElementById("total-price");
+  const checkoutBtn = document.getElementById("checkout-btn");
+
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `
+        <span>${product.name}-$${product.price.toFixed(2)}</span>
+        <button data-id=${product.id}>Add to cart</button>
+        `;
+    productList.appendChild(productDiv);
+  });
+  productList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const productId = parseInt(e.target.getAttribute("data-id"));
+      const product = products.find((p) => p.id === productId);
+      addToCart(product);
+    }
+  });
+
+  function addToCart(product) {
+    cart.push(product);
+    renderCart();
+  }
+  function renderCart() {
+    cartItems.innerText = "";
+
+    let totalPrice = 0;
+
+    if (cart.length > 0) {
+      emptyCartMessage.classList.add("hidden");
+      cartTotalMessage.classList.remove("hidden");
+
+      cart.forEach((item, index) => {
+        totalPrice += item.price;
+        const showCart = document.createElement("div");
+        showCart.innerHTML = `
+        ${item.name} - $${item.price.toFixed(2)}
+        `;
+        cartItems.appendChild(showCart);
+
+        totalPriceDisplay.innerText = `$${totalPrice.toFixed(2)}`;
+      });
+    } else {
+      emptyCartMessage.classList.remove("hidden");
+      totalPriceDisplay.innerText = `$0.00`;
+    }
+  }
+
+  checkoutBtn.addEventListener("click", () => {
+    cart.length = 0;
+    alert("checkout successfully");
+
+    renderCart();
+  });
+});
